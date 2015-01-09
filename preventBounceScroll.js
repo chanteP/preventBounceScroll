952542a9e56f3b973785c2cbcb1b2e7d4b2bedc6
chenwenbin03 <chenwenbin03@meituan.com>
2015-01-09 11:02:29 +0800

    commonjsify

--- preventBounceScroll.js
+++ preventBounceScroll.js
@@ -1,29 +1,41 @@
-;(function(window, document){
-    document.addEventListener('touchmove', function(e) {
-        notPreventScrollElement(e.target) || e.preventDefault();
-    }, false);
-    function notPreventScrollElement(element){
-        return isExtraElement(element) || isScrollElement(element);
+var notPreventScrollElement = function(element){
+    return isExtraElement(element) || isScrollElement(element);
+}
+//其他不爽的element
+var isExtraElement = function(element){
+    switch(true){
+        case element.tagName === 'INPUT' && element.type === 'range':;
+            return true;
+        default :
+            return false;
     }
-    function isExtraElement(element){
-        switch(true){
-            case element.tagName === 'INPUT' && element.type === 'range':;
-                return true;
-            default :
-                return false;
+}
+//能滚的element
+var isScrollElement = function(element) {
+    while(element) {
+        if (checkScrollElement(element)){
+            return element;
         }
+        element = element.parentElement;
     }
-    function isScrollElement(element) {
-        while(element) {
-            if (checkScrollElement(element)){
-                return element;
-            }
-            element = element.parentElement;
-        }
-        return false;
+    return false;
+}
+var checkScrollElement = function(element){
+    if(element.scrollHeight <= element.clientHeight){return false;}
+    //任性
+    return window.getComputedStyle(element)['-webkit-overflow-scrolling'] === 'touch';
+}
+
+//bind
+var bindFunc = function(e) {
+    notPreventScrollElement(e.target) || e.preventDefault();
+}
+
+module.exports = {
+    bind : function(){
+        document.addEventListener('touchmove', bindFunc, false);
+    },
+    destory : function(){
+        document.removeEventListener('touchmove', bindFunc, false);
     }
-    function checkScrollElement(element){
-        if(element.scrollHeight <= element.clientHeight){return false;}
-        return window.getComputedStyle(element)['-webkit-overflow-scrolling'] === 'touch';
-    }
-})(window, document);
+}
